e2b23a89100cfcca9b1b8d99c14eeafd3066fa12
Crowdbotics <team@crowdbotics.com>
2023-02-21 17:44:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-moon-39049",
+  url: "https://delicate-moon-39049.botics.co",
+  api: "https://delicate-moon-39049.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
